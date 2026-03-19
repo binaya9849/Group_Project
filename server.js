a991b22b47858ea1127a493fe428538e37776b35
binaya9849 <xandermaharjan@gmail.com>
2026-03-19 13:09:05 +0200

    installation of the files and folders

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ import app from "./app.js";
 
 
 app.listen(process.env.PORT,() => {
-    console.log("Server is running on PORT $(process.env.PORT")
+    console.log(`Server is running on PORT ${process.env.PORT}`);
 })
