0affe7de5119eb5d7962d4b7299c8f4b3e7daf47
binaya9849 <xandermaharjan@gmail.com>
2026-03-19 13:09:05 +0200

    installation of the files and folders

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
 import express from "express";
-import {config} from "dotenv";
+import { config } from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
 
 const app = express();
 
-config({path:"./config/config.env"});
+config({ path: "./config/config.env" });
 
-app.use(cors)(({
-    origin:[process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
-    methods: ["GET","PSOT","PUT","DELETE"],
-    credentials:true,
-})
+// CORS (FIXED)
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
 );
 
-app.listen(cookieParser());
+// Middlewares
+app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({extended : true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.use(fileUpload({
-    tempFileDir:"./uploads",
-    useTempFiles : true,
-    
-})
+app.use(
+  fileUpload({
+    tempFileDir: "./uploads",
+    useTempFiles: true,
+  })
 );
 
 export default app;
