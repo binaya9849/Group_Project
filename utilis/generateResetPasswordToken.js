0ce098d2904dac71ec19e3fd23d6ec5559f20022
binaya9849 <xandermaharjan@gmail.com>
2026-04-06 18:59:42 +0300

    commit with backend + frontend

--- utilis/generateResetPasswordToken.js
+++
@@ -1,9 +0,0 @@
-import crypto from "crypto";
-
-export const generateResetPasswordToken = () => {
-	const resetToken = crypto.randomBytes(32).toString("hex");
-	const hashedToken = crypto.createHash("sha256").update(resetToken).digest("hex");
-	const resetPasswordExpireTime = Date.now() + 10 * 60 * 1000;
-
-	return { resetToken, hashedToken, resetPasswordExpireTime };
-};
