530b0179b985fa865a7fa013ffad5a24a2e55617
Your Name <saurav@example.com>
2026-04-24 20:11:15 +0300

    fix cart sidebar toggle and update cart logic

--- frontend/src/store/slices/cartSlice.js
+++ frontend/src/store/slices/cartSlice.js
@@ -5,9 +5,41 @@ const cartSlice = createSlice({
   initialState: {
     cart: [],
   },
-  reducers: {},
+  reducers: {
+    addtoCart(state, action) {
+      const{ product, quantity} = action.payload;
+
+      const existingItem = state.cart.find(
+        item => item.product.id === product.id
+      );
+
+      if (existingItem){
+        existingItem.quantity += quantity;
+      } else {
+        state.cart.push({ product, quantity });
+      }
+    },
+    removeFromCart(state, action){
+      
+      state.cart = state.cart.filter(
+        item => item.product.id !== action.payload.id);
+    },
+    updateCartItem(state, action){
+      const item = state.cart.find(
+        item => item.product.id === action.payload.productId
+      );
+
+      if (item) {
+        item.quantity = action.payload.quantity;
+      }
+    },
+    clearCart(state) {
+      state.cart = [];
+    },
+
+  },
 });
 
-export const {} = cartSlice.actions;
+export const {addtoCart, removeFromCart, updateCartItem, clearCart} = cartSlice.actions;
 
 export default cartSlice.reducer;
